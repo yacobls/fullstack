3aef67b76123ee652e9771071da748d6b875480f
yacobls <yacob.lamrodo@gmail.com>
2023-01-16 11:16:27 +0700

    Add Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
+import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import PkpList from "./components/PkpList";
 import AddPkp from "./components/AddPkp";
 import EditPkp from "./components/EditPkp";
+import NavbarComponent from "./components/Navbar";
 
 function App() {
   return (
+      
     <BrowserRouter>
+      <NavbarComponent/>
       <Routes>
         <Route path="/" element={<PkpList/>}/>
         <Route path="add" element={<AddPkp/>}/>
         <Route path="edit/:id" element={<EditPkp/>}/>
-
       </Routes>
     </BrowserRouter>
+
   );
 }
 
