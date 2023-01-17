75c56b28d3b652fe5fa0fbfbdc8435022ff163cd
yacobls <yacob.lamrodo@gmail.com>
2023-01-17 11:02:53 +0700

    Edit index.css (frontend)

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
-import { Col } from 'react-bootstrap'
+import { Col, ListGroup, Button } from 'react-bootstrap'
+import './index.css';
 
 export default class List extends Component {
-  render() {
-    return (
-      <Col md={2} mt="2">
-        <h4><strong>Page Menu</strong></h4>
-        <hr />  
-      </Col>
-    )
-  }
+    render() {
+        return (
+            <Col md={2} mt="2">
+                <h4><strong>Page Menu</strong></h4>
+                <hr />
+                <ListGroup>
+                    <ListGroup.Item>Pelayanan</ListGroup.Item>
+                    <ListGroup.Item>Dashboard</ListGroup.Item>
+                    <Button className='btn-page-menu' href="edit/9">Kantor Pajak</Button>
+                    <ListGroup.Item>Wajib Pajak</ListGroup.Item>
+                    <ListGroup.Item>Alat Fiskal Elektronik</ListGroup.Item>
+                    <ListGroup.Item>Pelaporan</ListGroup.Item>
+                </ListGroup>
+            </Col>
+        )
+    }
 }
